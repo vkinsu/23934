0f6b3fb35e0876ae021db2e924d93ed91676f677
Nikita Shestakov <n.shestakov@g.nsu.ru>
2024-10-13 14:05:15 +0700

    task4 and gitignore

--- N.Shestakov/test.c
+++ N.Shestakov/test.c
@@ -1,17 +1,37 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+typedef struct List {
+    char* string;
+    int next;
+}List;
 
 int main() {
-    // Получаем значение переменной окружения TZ
-    setenv("TZ", "America/Los_Angeles", 1);
-    const char *tz = getenv("TZ");
+    int current_size = 10;
+    char buffer[100] = { 0 };
+    List* str_list = (List*)malloc(sizeof(List) * current_size);
+    int count = 0;
 
-    if (tz) {
-        printf("Текущая временная зона (TZ): %s\n", tz);
-    } else {
-        printf("Переменная окружения TZ не установлена.\n");
+    while (count < 1) {
+        printf("Введите строку: ");
+        if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+
+            str_list[count].string = (char*)malloc(sizeof(char) * strlen(buffer));
+            str_list[count].string = buffer;
+            count++;
+            if (current_size == count) {
+                current_size *= 2;
+                str_list = (List*)realloc(str_list, sizeof(List) * current_size);
+            }
+            if (count > 0){
+                str_list[count - 1].next = count;
+            }
+        }
     }
+    int len = 5;
+    printf("%d", len);
 
     return 0;
 }
