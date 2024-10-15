7c4591bee5c82a779251dafc6be5f034a67e53ec
Zavialov Stepan <s.zavyalov1@g.nsu.ru>
2024-10-15 10:30:18 +0700

    tasks_2-7

--- S.ZaVialoV/3.c
+++ S.ZaVialoV/3.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(){
+    uid_t real = getuid();
+    uid_t effective = geteuid();
+    
+    printf("real: %u\n", real);
+    printf("effective: %u\n", effective);
+    
+    FILE* file = fopen("test3.txt", "r");
+    if(file == NULL){
+        perror("fopen");
+        exit(0);
+    }
+    else
+        fclose(file);
+    
+    setuid(real);
+    effective = geteuid();
+    printf("real: %u\n", real);
+    printf("effective: %u\n", effective);
+    
+    file = fopen("test3.txt", "r");
+    if(file == NULL){
+        perror("fopen");
+        exit(0);
+    }
+    else
+        fclose(file);
+    
+    return 0;
+}
--- S.ZaVialoV/5.c
+++ S.ZaVialoV/5.c
@@ -1,42 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <fcntl.h>
 
 int main(){
-    int in = open("test5.txt", O_RDONLY);
-    
-    int n = 1;
-    long long* arr = malloc(1000 * sizeof(long long));
-    arr[0] = 0;
-
-    long long i=0;
-    char c;
-    
-    while(read(in, &c, 1)){
-        if(c == '\n') arr[n++] = i+1;
-        i++;
+    int in = open("test.txt", O_RDONLY);
+    if(in == -1){
+        perror("test.txt (analysis)");
+        exit(0);
     }
-    arr[n++] = i-1;
-    close(in);
+    
+    char c;
+    int n = 0;
+    long long* arr = malloc(1000 * sizeof(long long));
+    arr[n++] = 0;
+    
+    if(read(in, &c, 0) == -1){
+        perror("read (analysis)");
+        exit(0);
+    }
+    for(long long i=0; read(in, &c, 1); i++){
+        if(c == '\n'){
+            arr[n++] = i+1;
+        }
+    }
+    arr[--n] = 0;
+    
+    if(close(in) == -1){
+        perror("close (analysis)");
+        exit(0);
+    }
     
     int num;
     printf("Enter the line number.\n");
-    scanf("%d", &num);
+    
+    if(scanf("%d", &num) == 0){
+        fprintf(stderr, "Wrong type.\n");
+        exit(0);
+    }
     
     if(num > 0 && num <= n){
-        in = open("test5.txt", O_RDONLY);
-        lseek(in, arr[num-1], 0);
+        in = open("test.txt", O_RDONLY);
+        if(in == -1){
+            perror("open (searching)");
+            exit(0);
+        }
+        
+        if(lseek(in, arr[num-1], 0) == -1){
+            perror("lseek");
+            exit(0);
+        }
+        
+        if(read(in, &c, 0) == -1){
+            perror("read (searching)");
+            exit(0);
+        }
         read(in, &c, 1);
         
         while(c != '\n' && c != 0){
             printf("%c", c);
             read(in, &c, 1);
         }
+        printf("\n");
         
-        close(in);
+        if(close(in) == -1){
+            perror("close");
+            exit(0);
+        }
     }
+    else if(num != 0){
+        fprintf(stderr, "Wrong index.\n");
+        exit(0);
+    }
+    else
+        printf("ok\n");
     free(arr);
     return 0;
 }
