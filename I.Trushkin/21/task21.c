ef5477493a106cc39e72fda3c6cab013a0e55c62
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-11-12 21:02:11 +0700

    fix 21

--- I.Trushkin/21/task21.c
+++ I.Trushkin/21/task21.c
@@ -10,27 +10,27 @@ const char *reset = "\x1b[0m";
 const char *green = "\x1b[32m";
 const char *yellow = "\033[33m";
 
-void handler_SIGINT_squeak (int signum) {
+void handler_SIGINT_squeak(int signum) {
     count++;
     write(STDOUT_FILENO, "\a", 1); // Издает звуковой сигнал
 }
 
-void handler_SIGQUIT_exit (int signum) {
-    printf("\n%s The signal sounded: %d once \n%s",green, count, reset);
+void handler_SIGQUIT_exit(int signum) {
+    printf("\n%s The signal sounded: %d once \n%s", green, count, reset);
     exit(0);
 }
 
 int main() {
     printf("%sThis program beeps in the terminal every time,\n", yellow);
     printf("when the SIGINT signal (Ctr-C) is sent.\n");
-    printf("The program will exit when it receives SIGQUIT (Ctr-\\).\n%s",reset);
+    printf("The program will exit when it receives SIGQUIT (Ctr-\\).\n%s", reset);
 
     count = 0;
-    signal(SIGINT, handler_SIGINT_squeak);
-    signal(SIGQUIT, handler_SIGQUIT_exit);
 
-    while(1) {
+    while (1) {
         pause();
+        signal(SIGINT, handler_SIGINT_squeak);
+        signal(SIGQUIT, handler_SIGQUIT_exit);
     }
     return 0;
 }
