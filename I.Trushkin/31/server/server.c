3b95910191202f5e0214ddb18bf46d0ebce582cf
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-11-16 00:27:19 +0700

    add 31

--- I.Trushkin/31/server/server.c
+++ I.Trushkin/31/server/server.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <ctype.h>
+#include <signal.h>
 #define MAX_LENGTH_TEXT 40
 
 const char *red = "\033[31m";
@@ -13,53 +14,87 @@ const char *reset = "\033[0m";
 const char *green = "\033[32m";
 const char *purple = "\033[35m";
 const char *yellow = "\033[33m";
+int flagWhile = 0;
 
 
+void handle_signal(int sig) {
+    flagWhile = 1;
+}
 
 int main() {
-    int sock = socket(AF_UNIX,SOCK_STREAM,0);
+
+    signal(SIGINT, handle_signal);
+
+
+    int sock = socket(AF_UNIX,SOCK_STREAM, 0);
+    int new_sock;
+    fd_set active_set, read_set;
+    struct sockaddr_un addr;
+    struct sockaddr_un client_addr;
+    int size;
+    char text[MAX_LENGTH_TEXT];
+
 
     if (sock == -1) {
         printf("%sError: failed to create socket %s\n", red, reset);
         exit(1);
     }
 
-    struct sockaddr_un addr;
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, "socket",sizeof(addr.sun_path)-1);
 
-    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr))==-1) {
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, "socket", sizeof(addr.sun_path) - 1);
+
+    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
         printf("%sError: failed to set socket address %s\n", red, reset);
         exit(1);
     }
-    
-    if (listen(sock,1) ==-1){
+
+    if (listen(sock, 3) == -1) {
         printf("%sError: failed to listen socket %s\n", red, reset);
         exit(1);
     }
-    
+
+    FD_ZERO(&active_set);
+    FD_SET(sock, &active_set);
+
+
     printf("%sThe server ready to listen%s\n", green, reset);
-    
-    
-    struct sockaddr_un client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    int newSock = accept(sock, NULL, NULL);
-    if (newSock == -1){
-        printf("%sError: failed to accept %s\n", red, reset);
-    }
-    
-    char text[MAX_LENGTH_TEXT];
-    
-    while (1) {
-        ssize_t bytesRead = read(newSock, text, MAX_LENGTH_TEXT);
-        if (bytesRead <= 0) break; 
-        text[bytesRead] = '0'; 
-        for (int i = 0; i < bytesRead; i++) {
-                text[i] = toupper(text[i]); 
+
+
+    while (!flagWhile) {
+        read_set = active_set;
+
+        if (select(FD_SETSIZE, &read_set, NULL, NULL, NULL) < 0) {
+            printf("%sError: failed to select failure %s\n", red, reset);
+            exit(1);
         }
-        printf("%s%s%s\n", purple, text, reset); 
-    } 
+
+        for (int i = 0; i < FD_SETSIZE; i++) {
+            if (FD_ISSET(i, &read_set)) {
+                if (i == sock) {
+                    size = sizeof(client_addr);
+                    new_sock = accept(sock, NULL, NULL);
+                    if (new_sock == -1) {
+                        printf("%sError: failed to accept %s\n", red, reset);
+                        exit(1);
+                    }
+                    FD_SET(new_sock, &active_set);
+                }
+            } else {
+                ssize_t bytesRead = read(new_sock, text, MAX_LENGTH_TEXT);
+                if (bytesRead <= 0) {
+                    close(new_sock);
+                    FD_CLR(new_sock, &active_set);
+                }
+                text[bytesRead] = '\0';
+                for (int i = 0; i < bytesRead; i++) {
+                    text[i] = toupper(text[i]);
+                }
+                printf("%s%s%s\n", purple, text, reset);
+            }
+        }
+    }
+
     unlink("socket");
     close(sock);
 }
-
