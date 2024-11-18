7cb93b05050bd16fd060a0108dfb845806ab51c7
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-11-19 03:23:58 +0700

    fix names

--- I.Trushkin/10/task10.c
+++ I.Trushkin/10/task10.c
@@ -13,33 +13,26 @@ const char *green = "\x1b[32m";
 const char *purple = "\x1b[35m";
 
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        printf("%sError: no arguments entered %s\n", red, reset);
+    }
     pid_t pid;
-    char filename[FILE_NAME_LEN];
-
+    int status;
     switch (pid = fork()) {
         case -1:
-            printf("%sError: failed to create subprocess %s", red, reset);
+            printf("%sError: failed to create subprocess %s\n", red, reset);
             exit(1);
         case 0:
-            printf("%sCHILD: This is a child process!\n", green, reset);
-            printf("Enter file name, please:\n");
-            if (fgets(filename, FILE_NAME_LEN, stdin) == NULL) {
-                printf("%sError: failed to read file name%s", red, reset);
-                exit(1);
-            } else {
-                size_t len = strlen(filename);
-                if (len > 0 && filename[len - 1] == '\n') {
-                    filename[len - 1] = '\0';
-                }
-            }
-            execlp("cat", "cat", filename, (char *) 0);
+            printf("%sCHILD: This is a child process!%s \n", green, reset);
+            execvp(argv[1], &argv[1]);
             printf("%sError: failed to replace process %s", red, reset);
             exit(1);
 
         default:
             printf("%sPARENT: This is a parent process!%s\n", purple, reset);
             wait(NULL);
+            printf("\n%s%d%s", purple, WEXITSTATUS(status), reset);
             printf("\n%sThe parent waited for the subprocess to complete!\n%s",purple,reset);
     }
     return 0;
