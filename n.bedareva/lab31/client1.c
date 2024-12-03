2d1f4bddc2810dea4ce589dcfc51debf5d984dce
NatashaBedareva <116941649+NatashaBedareva@users.noreply.github.com>
2024-12-03 10:16:12 +0700

    31

---
+++ n.bedareva/lab31/client1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "/tmp/unix_socket"
+#define BUFFER_SIZE 256
+
+int main() {
+    int client_fd;
+    struct sockaddr_un addr;
+    char buffer[BUFFER_SIZE];
+
+    client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (client_fd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    if (connect(client_fd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+        perror("connect");
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Conected server\n");
+
+    while (1) {
+        fgets(buffer, sizeof(buffer), stdin);
+        send(client_fd, buffer, strlen(buffer), 0);
+    }
+    
+    close(client_fd);
+    return 0;
+}
