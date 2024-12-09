700ca4e8b466f75a5afd903dfcaa1f800f2f9f89
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-12-09 21:45:22 +0700

    fix 32_* client

--- I.Trushkin/32/client/client.c
+++ I.Trushkin/32/client/client.c
@@ -89,7 +89,7 @@ int main() {
         } else {
             read(client_sock, tempText, sizeof(tempText) - 1);
             tempText[strlen(tempText)] = '!';
-            printf("%d\n", strlen(tempText));
+            printf("%s\n", tempText);
             write(client_sock, tempText, strlen(tempText) + 1);
         }
     }
