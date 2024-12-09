858621b09794c2623e5db5879da931adeffa8612
ilushka-obnimashka <i.trushkin@g.nsu.ru>
2024-12-09 20:11:36 +0700

    add 32_*

---
+++ I.Trushkin/32/client/client.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <ctype.h>
+#include <time.h>
+
+
+#define MAX_LENGTH_TEXT 40
+
+const char *red = "\033[31m";
+const char *reset = "\033[0m";
+const char *green = "\033[32m";
+const char *purple = "\033[35m";
+const char *yellow = "\033[33m";
+
+
+int main() {
+    int client_sock = socket(AF_UNIX,SOCK_STREAM,0);
+
+    if (client_sock == -1) {
+        printf("%sError: failed to create socket %s\n", red, reset);
+        exit(1);
+    }
+
+    printf("%sThe client ready to connect%s\n", green, yellow);
+
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, "../server/socket" ,sizeof(addr.sun_path));
+
+    if (connect(client_sock,(struct sockaddr *)&addr,sizeof(addr))==-1){
+        printf("%sError: failed to connect server%s\n", red, reset);
+        exit(1);
+    }
+
+    enum class numbCklient {
+        first, second
+    };
+    enum numbCklient id_client;
+
+    char tempText[MAX_LENGTH_TEXT];
+
+    read(client_sock, tempText, strlen(tempText) + 1);
+    if (tempText == "first\0") {
+        id_client = first;
+    }
+    else {
+        id_client = second;
+    }
+
+    enum class isMessage {
+        not_received,received
+    };
+    enum isMessage flag = not_received;
+
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = 500000000;
+
+    while (1) {
+        if (id_client == first){
+            if (flag == not_received) {
+                if (fgets(tempText, sizeof(tempText), stdin) == NULL) break; // Проверка на EOF
+                if (strlen(tempText) > MAX_LENGTH_TEXT - 1) {
+                    printf("%sERROR: The entered text is too long! Maximum %d characters.%sn", red, MAX_LENGTH_TEXT - 1, reset);
+                    continue;
+                }
+                size_t len = strlen(tempText);
+                if (len > 0 && tempText[len - 1] == '\n') {
+                    tempText[len - 1] = '\0';
+                }
+                flag = received;
+            }
+            write(client_sock, tempText, strlen(tempText) + 1);
+            nanosleep(&ts, NULL);
+        }
+        else {
+            read(client_sock, tempText, strlen(tempText) + 1);
+            tempText[strlen(tempText)] = '!';
+            write(client_sock, tempText, strlen(tempText) + 1);
+        }
+    }
+    close(client_sock);
+}
+
---
+++ I.Trushkin/32/server/server.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <ctype.h>
+#include <signal.h>
+#define MAX_LENGTH_TEXT 40
+
+const char *red = "\033[31m";
+const char *reset = "\033[0m";
+const char *green = "\033[32m";
+const char *purple = "\033[35m";
+const char *yellow = "\033[33m";
+
+int main() {
+    unlink("socket");
+
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    int new_sock;
+
+    struct sockaddr_un addr;
+    struct sockaddr_un client_addr;
+
+    char text[MAX_LENGTH_TEXT];
+
+    fd_set active_set, read_set;
+
+    int size;
+
+    if (sock == -1) {
+        printf("%sError: failed to create socket %s\n", red, reset);
+        exit(1);
+    }
+
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, "socket", sizeof(addr.sun_path) - 1);
+
+    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+        printf("%sError: failed to set socket address %s\n", red, reset);
+        exit(1);
+    }
+
+    if (listen(sock, 2) == -1) {
+        printf("%sError: failed to listen socket %s\n", red, reset);
+        exit(1);
+    }
+
+    FD_ZERO(&active_set);
+    FD_SET(sock, &active_set);
+    int max_fd = sock;
+
+    printf("%sThe server ready to listen%s\n", green, reset);
+
+    char **numb_client = (char **)malloc(2 * sizeof(char *));
+    numb_client[0] = (char *)malloc(6 * sizeof(char)); // "first" + '\0'
+    numb_client[1] = (char *)malloc(7 * sizeof(char)); // "second" + '\0'
+
+    char id_client [][]
+
+    while (1) {
+        read_set = active_set;
+
+        if (select(max_fd + 1, &read_set, NULL, NULL, NULL) < 0) {
+            printf("%sError: failed to select failure %s\n", red, reset);
+            exit(1);
+        }
+
+        for (int i = 0; i <= max_fd; i++) {
+            if (FD_ISSET(i, &read_set)) {
+                if (i == sock) {
+                    new_sock = accept(sock, NULL, NULL);
+                    if (new_sock == -1) {
+                        printf("%sError: failed to accept %s\n", red, reset);
+                        exit(1);
+                    } else {
+                        FD_SET(new_sock, &active_set);
+                        if (new_sock > max_fd) {
+                            max_fd = new_sock;
+                        }
+                    }
+                    if (numb_client[0] != NULL) {
+                        write (i,numb_client[0],strlen(numb_client[0]));
+                        numb_client[0] = NULL;
+                    }
+                    else {
+                        write (i,numb_client[1],strlen(numb_client[1]));
+                    }
+                } else {
+                    ssize_t bytesRead = read(i, text, MAX_LENGTH_TEXT - 1);
+                    if (bytesRead <= 0) {
+                        close(i);
+                        FD_CLR(i, &active_set);
+                    } else {
+                        text[bytesRead] = '\0';
+                        printf("%s%s%s\n", purple, text, reset);
+                    }
+                }
+            }
+        }
+    }
+    unlink("socket");
+    close(sock);
+}
