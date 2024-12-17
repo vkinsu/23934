bfb5a3489e90f4a4f904bbad85bfb2acc116da7c
Zavialov Stepan <s.zavyalov1@g.nsu.ru>
2024-12-17 10:22:38 +0700

    tasks 1, 17, 18, 25

--- S.ZaVialoV/21/21.c
+++ S.ZaVialoV/21/21.c
@@ -5,22 +5,26 @@
 
 int count = 0;
 
-void handle_signal(int sig){
-    if (sig == SIGQUIT){
+void handle_signal(int sig)
+{
+    if (sig == SIGQUIT)
+    {
         char buf[50];
         int len = snprintf(buf, sizeof(buf), "\nBeep sounded: %d times\n", count);
         write(STDOUT_FILENO, buf, len);
         exit(0);
     }
 
-    else if (sig == SIGINT){
+    else if (sig == SIGINT)
+    {
         write(STDOUT_FILENO, "\007", 1);
         count++;
         signal(SIGINT, handle_signal);
     }
 }
 
-int main(){
+int main()
+{
     signal(SIGINT, handle_signal);
     signal(SIGQUIT, handle_signal);
 
